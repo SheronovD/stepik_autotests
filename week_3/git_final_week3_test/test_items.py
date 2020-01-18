cdfecc31cb4809463710a68810620f43917fd9e0
SheronovD <sheronovd@inbox.ru>
2020-01-19 01:16:27 +0300

    assert find_button fix

--- week_3/git_final_week3_test/test_items.py
+++ week_3/git_final_week3_test/test_items.py
@@ -2,11 +2,9 @@ import time
 from selenium.common.exceptions import NoSuchElementException
 
 def test_is_exist_add_to_basket_btn(browser):
-	try:
-		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-		browser.get(link)
-		time.sleep(5)
-		browser.find_element_by_css_selector("#add_to_basket_form > button")
-
-	except NoSuchElementException:
-		assert False , "Button doesn't exist!"
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+	browser.get(link)
+	time.sleep(5)
+	is_btn_exist = len(browser.find_elements_by_css_selector("#add_to_basket_form > button"))
+	
+	assert is_btn_exist == 1 , "Button doesn't exist!"
